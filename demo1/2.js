81d7cb4dbfb3657fa9f3e888f50a357df7eb16ce
swara-2006 <swara632006@gmail.com>
2026-02-05 22:37:35 +0530

    2nd commit

---
+++ demo1/2.js
@@ -0,0 +1,35 @@
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import Lenis from "lenis";
+
+gsap.registerPlugin(ScrollTrigger);
+
+document.addEventListener("DOMContentLoaded", () => {
+  const lenis = new Lenis();
+
+  lenis.on("scroll", ScrollTrigger.update);
+
+  gsap.ticker.add((time) => {
+    lenis.raf(time * 1000);
+  });
+
+  gsap.ticker.lagSmoothing(0);
+
+  const path = document.getElementById("stroke-path");
+  const pathLength = path.getTotalLength();
+
+  path.style.strokeDasharray = pathLength;
+  path.style.strokeDashoffset = pathLength;
+
+    gsap.to(path, {
+  strokeDashoffset: 0,
+  ease: "none",
+  scrollTrigger: {
+    trigger: ".spotlight",
+    start: "top top",
+    end: "bottom bottom",
+    scrub: true,
+  },
+});
+
+});
